df9ba66eb0b71019b489b241a5f1fb0e12604d2e
DanieleConsoli97 <Danyconsoli4@gmail.com>
2025-01-17 17:47:21 +0100

    end exercise

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 // Consegna
 // Inseriamo in pagina l’immagine della lampadina spenta che trovate in allegato e accanto un bottone con la scritta “Accendi”.
 // Al click del bottone, la lampadina dovrà accendersi (dovremo quindi utilizzare l’immagine della lampadina accesa, sempre in allegato)
+
+
+var lampadina = document.getElementById("imgLamp")
+
+var bottone = document.getElementById("buttonOn")
+let onOff = false
+bottone.addEventListener("click",()=>{
+    if (onOff== false) {
+        lampadina.src = "./img-5/yellow_lamp.png";
+        onOff=true
+        bottone.innerHTML = "Spegni la lampadina" 
+    } else {
+        lampadina.src = "./img-5/white_lamp.png";
+        onOff = false
+        bottone.innerHTML = "Accendi la lampadina" 
+    }
+   
+})
